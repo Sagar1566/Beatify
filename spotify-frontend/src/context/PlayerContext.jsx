bea249f24830539b40b3c6a2ca15660546d1816f
Sagar1566 <gajbharsagar2005@gmail.com>
2025-05-07 01:01:16 +0530

    done

--- spotify-frontend/src/context/PlayerContext.jsx
+++ spotify-frontend/src/context/PlayerContext.jsx
@@ -5,7 +5,7 @@ export const PlayerContext = createContext();
 
 const PlayerContextProvider = (props) => {
 
-    const url = 'http://localhost:3000';
+    const url = 'https://beatify-1.onrender.com';
 
     const audioRef = useRef();
     const seekBar = useRef();
